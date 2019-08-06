50a202b892065b7dc7ba0b5faa3053e7e0200328
DMK <dmkshift@gmail.com>
2019-08-06 11:24:30 +0300

    fixing

--- codevog_test/settings.py
+++ codevog_test/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    'time_tracker/static',
 )
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
